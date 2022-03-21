7ca25c718d65d56b82d2d4cf206b7a0596796529
Srivatsa Joshi <srivatsajoshi19@gmail.com>
2022-03-21 19:57:13 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ParentComponent } from './components/parent/parent.component';
+import { ChildComponent } from './components/child/child.component';
+import { DisplayComponent } from './components/display/display.component';
+import { DisplayChildComponent } from './components/display-child/display-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChildComponent,
+    DisplayComponent,
+    DisplayChildComponent
   ],
   imports: [
     BrowserModule
